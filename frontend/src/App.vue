e577e284a7c9c1ab9f07385786a8a45f1655fb01
jubrother <jubrother@naver.com>
2020-10-27 14:57:30 +0900

    S03P31A306-31 | frontend | Map

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,50 @@
 <template>
   <div id="app">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>매치ON</title>
+    </head>
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/map">Map</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: "app",
+  mounted() {
+    this.addScript();
+  },
+  methods: {
+    addScript() {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load();
+      script.src =
+        "//dapi.kakao.com/v2/maps/sdk.js?appkey=be80825bde1c9ecb6216babea86cf0ea&autoload=false&libraries=services,clusterer,drawing";
+      document.head.appendChild(script);
+    },
+  },
+};
+</script>
+<style scoped>
+@import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+
+.notosanskr * {
+  font-family: "Noto Sans KR", sans-serif;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: auto;
 }
 
 #nav {
@@ -28,5 +58,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+* {
+  font-family: "Noto Sans KR", sans-serif;
 }
 </style>
