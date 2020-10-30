d1ba38ece799395d6cd10c607650bf0314d7d5fc
박세종 <tpwhd95@gmail.com>
2020-10-30 17:58:57 +0900

    S03P31A306-41 | backend

--- backend/backend/sql_settings.py
+++ backend/backend/sql_settings.py
@@ -8,3 +8,14 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'matchon',
+#         'USER': 'root',
+#         'PASSWORD': 'ssafy306',
+#         'HOST': 'k3a306.p.ssafy.io',
+#         'PORT': '3306',
+#     }
+# }
