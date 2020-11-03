e71d1cf3a634bb71f7b3be4e7f94358b8c47c7c1
박세종 <tpwhd95@gmail.com>
2020-11-03 15:08:26 +0900

    sql hotfix

--- backend/backend/sql_settings.py
+++ backend/backend/sql_settings.py
@@ -1,21 +1,21 @@
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'matchon',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default' : {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'matchon',
 #         'USER': 'root',
-#         'PASSWORD': 'ssafy306',
-#         'HOST': 'k3a306.p.ssafy.io',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'matchon',
+        'USER': 'root',
+        'PASSWORD': 'ssafy306',
+        'HOST': 'k3a306.p.ssafy.io',
+        'PORT': '3306',
+    }
+}
