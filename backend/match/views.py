10aac848433b91df88941bebd156acb0d4eb6cfd
조원빈 <cwb5116@naver.com>
2020-11-05 17:04:17 +0900

    S03P31A306-44 | updated match.views

--- backend/match/views.py
+++ backend/match/views.py
@@ -8,7 +8,8 @@ from rest_framework.decorators import api_view
 from .match_funcs import re_geocode
 
 # 모델
-from users.models import BeforeMatch
+from users.models import BeforeMatch, AfterMatch
+from match.models import Sports, Match, MatchUser
 
 # 시리얼라이저
 from .serializers import BMSerializer
@@ -57,13 +58,13 @@ def before_match(request):
             # date = '2020-11-04',
             # start_time = '15:21:32',
             # end_time = '15:21:32',
-            date = datetime.date.today(),
-            start_time = (datetime.datetime.now() - datetime.timedelta(hours= 3)).strftime("%H:%M"),
-            end_time = datetime.datetime.now().time(),
+            # date = datetime.date.today(),
+            # start_time = (datetime.datetime.now() - datetime.timedelta(hours= 3)).strftime("%H:%M"),
+            # end_time = datetime.datetime.now().time(),
 
-            # date = data['date'],
-            # start_time = data['start_time'],
-            # end_time = data['end_time'],
+            date = data['date'],
+            start_time = data['start_time'],
+            end_time = data['end_time'],
 
             lat = data['lat'],
             lng = data['lng'],
@@ -73,42 +74,7 @@ def before_match(request):
         serializer = BMSerializer(bm_match)
 
 
-        # 첫 번째 탐색법
         # 매칭 전 / 종목 이름 / 날짜 / 구 이름이 같은 조건 탐색
-        # crnt_bm_matches = BeforeMatch.objects.filter(status = 1, sports_name = bm_match.sports_name, date = bm_match.date, gu = bm_match.gu).exclude(user = bm_match.user)
-        # sports_count = {'tennis': 2, 'pool': 2, 'bowl': 2, 'basket_ball': 6, 'futsal': 12}
-        
-        # # 해당 스포츠의 같은 동네에서 인원 수가 충족되고
-        # if crnt_bm_matches.count() >= sports_count[bm_match.sports_name] - 1:
-        #     # 시간도 겹치는지 확인해봅시다.
-        #     match_users = [set() for _ in range(24)]
-        #     # 현재 사용자의 정보를 집어넣고
-        #     stime_idx = int(bm_match.start_time[:2])
-        #     etime_idx = int(bm_match.end_time.strftime("%H:%M")[:2])
-            
-        #     for i in range(stime_idx, etime_idx + 1):
-        #         match_users[i].add(bm_match.pk)
-            
-        #     matched = []
-        #     # 다른 사람들의 정보를 집어넣는다.
-        #     for user_bm in crnt_bm_matches.order_by('pk'):
-        #         stime_idx = int(user_bm.start_time.strftime("%H:%M")[:2])
-        #         etime_idx = int(user_bm.end_time.strftime("%H:%M")[:2])
-                
-        #         for i in range(stime_idx, etime_idx + 1):
-        #             if len(match_users[i]) >= sports_count[bm_match.sports_name] - 1:
-        #                 print(match_users[i])
-        #                 # matched.append(match_users[i])
-        #                 # 현재 넣고 있는 유저의 넣었던 내용 빼기
-
-        #                 # 매칭된 유저의 내용 빼기
-        #             else:
-        #                 match_users[i].add(user_bm.pk)
-        # else:
-        #     print('Naah')
-
-
-        # 두 번째 탐색법 -> 모두 찾기!
         crnt_bm_matches = BeforeMatch.objects.filter(status = 1, sports_name = bm_match.sports_name, date = bm_match.date, gu = bm_match.gu)
         sports_count = {'tennis': 2, 'pool': 2, 'bowl': 2, 'basket_ball': 6, 'futsal': 12}
         
@@ -145,12 +111,39 @@ def before_match(request):
                         match_users[i].add(user_bm.pk)
             print('match_users:', match_users)
             print('matched: ', matched)
-        else:
-            print('Naah')
 
 
+            # 잡혀진 매치가 있다면 해당 매치를 게임으로 바꿔줘야겠죠.
+            while matched:
+                # BeforeMatch PK가 들어가 있는 리스트
+                bm_pks = matched.pop()
+                print('players: ', bm_pks)
+                
+                # 매치를 잡아줍니다.
+                sports_name = get_object_or_404(Sports, sports_name=bm_match.sports_name)
+                match = Match(sports = sports_name)
+                match.save()
+
+                # 유저 정보를 꺼내서 AfterMatch와 MatchUser를 만들어 줍니다.
+                for bm_pk in bm_pks:
+                    bm = get_object_or_404(BeforeMatch, pk=bm_pk)
+                    bm.status = 2
+                    bm.save()
+                    
+                    # MatchUser를 저장해줍니다.
+                    mmatch_user = MatchUser(
+                        match = match,
+                        user_pk = bm.user.pk
+                    )
+                    mmatch_user.save()
+
+                    # AfterMatch를 만들어줍니다.
+                    am = AfterMatch(
+                        before_match = bm,
+                        matching_pk = match.pk
+                    )
+                    am.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
     else:
-        msg = {"status_code": 403, "detail": "겹치는 시간이 있습니다."}
+        msg = {"status_code": 403, "detail": "이미 해당 시간에 매칭 중인 게임이 있습니다."}
         return Response(msg)
