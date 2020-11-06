93176b1ed05455d5e1a828d91e5eef6de30991ad
이진석 <qlraor09@naver.com>
2020-11-06 15:14:15 +0900

    hotfix | setting

--- backend/backend/sql_settings.py
+++ backend/backend/sql_settings.py
@@ -14,19 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'matchon',
         'USER': 'root',
-        'PASSWORD': 'ssafy',
-        'HOST': 'localhost',
+        'PASSWORD': 'ssafy306',
+        'HOST': 'k3a306.p.ssafy.io',
         'PORT': '3306',
     }
 }
-
-# DATABASES = {
-#     'default' : {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'matchon',
-#         'USER': 'root',
-#         'PASSWORD': 'ssafy306',
-#         'HOST': 'k3a306.p.ssafy.io',
-#         'PORT': '3306',
-#     }
-# }
