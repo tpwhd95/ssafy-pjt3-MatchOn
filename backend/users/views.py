88a321c6a9e3809ebc5a33e67b74736656af8291
조원빈 <cwb5116@naver.com>
2020-11-09 14:53:33 +0900

    S03P31A306-60 | MATCH API DONE

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 import requests
 import json
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
-from .models import User
-from .serializers import UserSerializer,UserSerializerWithToken
 
-from rest_auth.registration.views import SocialLoginView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_auth.registration.views import SocialLoginView
 import hashlib
 
+from .models import User, BeforeMatch, AfterMatch
+from .serializers import UserSerializer,UserSerializerWithToken
 
 class KakaoLogin(SocialLoginView):
     def post(self, request):
@@ -30,3 +31,33 @@ class KakaoLogin(SocialLoginView):
         user = User.objects.get(user_id = u[0].user_id)
         user.social_id = 0
         return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def match_info(request):
+    user = request.user
+    all_bms = BeforeMatch.objects.filter(user=request.user).order_by('-date', '-start_time')
+    data = []
+    for bm in all_bms:
+        am = get_object_or_404(AfterMatch, before_match=bm.pk)
+        match_dict = {}
+        match_dict['status'] = bm.status
+        match_dict['matching_pk'] = am.matching_pk
+        match_dict['sports_name'] = bm.sports_name
+        match_dict['date'] = bm.date
+        match_dict['start_time'] = bm.start_time
+        match_dict['end_time'] = bm.end_time
+        match_dict['gu'] = bm.gu
+        match_dict['team_pk'] = am.team_pk
+        match_dict['fixed_time'] = am.fixed_time
+        match_dict['fixed_lat'] = am.fixed_lat
+        match_dict['fixed_lng'] = am.fixed_lng
+        match_dict['result'] = am.result
+
+        data.append(match_dict)
+    # print(context)
+    context = {
+        'result' : 'true',
+        'data' : data
+    }
+    return Response(context, status=status.HTTP_200_OK)
