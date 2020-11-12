042bbc0511ac90f1f5a6dd83c4993d490c399694
jubrother <jubrother@naver.com>
2020-11-13 01:13:20 +0900

    S03P31A306-77 | chat | makeroom

--- frontend/src/views/MatchRoom.vue
+++ frontend/src/views/MatchRoom.vue
@@ -3,22 +3,49 @@
     <h1>조율하는 방</h1>
     <p>{{ match_id }}번 경기</p>
     <p>방장: {{ room_master }}</p>
+    <div class="card">
+      <div class="card-body">
+        <p class="text-secondary nomessages" v-if="messages.length == 0">
+          [No messages yet!]
+        </p>
+        <div class="messages" v-chat-scroll="{ always: false, smooth: true }">
+          <div v-for="message in messages" :key="message.id">
+            <span class="text-info">[{{ message.name }}]: </span>
+            <span>{{ message.message }}</span>
+            <br />
+            <span class="text-secondary time">{{ message.timestamp }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="card-action">
+        <CreateMessage :name="userProfile.username" :match_id="match_id" />
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
 import http from "@/util/http-common";
 import { mapState } from "vuex";
+import CreateMessage from "@/components/CreateMessage";
+import fb from "@/firebase/init";
+import moment from "moment";
 
 export default {
   name: "MatchRoom",
-  components: {},
+  components: {
+    CreateMessage,
+  },
   data() {
     return {
       match_id: this.$route.query.match_id,
       room_master: null,
       center_lat: "",
       center_lng: "",
+      messages: [],
+      userProfile: sessionStorage.getItem("userProfile")
+        ? JSON.parse(sessionStorage.getItem("userProfile"))
+        : [],
     };
   },
   computed: {
@@ -26,6 +53,24 @@ export default {
   },
   created() {
     this.getRoomData(this.match_id);
+
+    let ref = fb
+      .collection("messages" + String(this.match_id))
+      .orderBy("timestamp");
+
+    ref.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if ((change.type = "added")) {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            message: doc.data().message,
+            timestamp: moment(doc.data().timestamp).format("LTS"),
+          });
+        }
+      });
+    });
   },
   methods: {
     getRoomData(match_id) {
@@ -56,5 +101,28 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 0px;
+}
+
+.chat h5 {
+  margin-top: 0px;
+  margin-bottom: 40px;
+}
+
+.chat span {
+  font-size: 1.2em;
+}
+
+.chat .time {
+  display: block;
+  font-size: 0.7em;
+}
+
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
 </style>
