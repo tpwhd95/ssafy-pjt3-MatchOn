c64f0bcaa78e37a2c03c43e60518c8fe281b2467
박세종 <tpwhd95@gmail.com>
2020-11-15 23:45:26 +0900

    mobile/pc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,124 +1,125 @@
 <template>
   <v-app>
-    <!-- 내브바 -->
-    <v-app-bar app class="align-center" color="black darken-3" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <div style="height: 40px">
-        <!-- <h1 @click="$router.push('/')" class="ft-dh" style="cursor: pointer">
+    <div class="d-md-none h-100">
+      <!-- 내브바 -->
+      <v-app-bar app class="align-center" color="black darken-3" dark>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <div style="height: 40px">
+          <!-- <h1 @click="$router.push('/')" class="ft-dh" style="cursor: pointer">
           매치<span class="ft-dh onred">온</span>
         </h1> -->
-        <img
-          @click="$router.push('/')"
-          src="@/assets/images/logos/logo.png"
-          alt="match-on-logo"
-          style="height: 100%"
-          class="ml-auto"
-        />
-      </div>
+          <img
+            @click="$router.push('/')"
+            src="@/assets/images/logos/logo.png"
+            alt="match-on-logo"
+            style="height: 100%"
+            class="ml-auto"
+          />
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <!-- 비로그인 디비전 -->
-      <v-dialog v-if="!this.isLoggedIn" v-model="dialog" max-width="500">
-        <template v-slot:activator="{ on, attrs }">
-          <!-- <v-btn color="teal" dark v-bind="attrs" v-on="on"> 로그인 </v-btn> -->
-          <i v-bind="attrs" v-on="on" class="fas fa-power-off fa-lg"></i>
-        </template>
-        <v-card style="padding: 20px">
-          <v-card-title
-            class="headline d-flex justify-center ft-dh"
-            style="padding: 0px 0px 20px 0px"
-          >
-            로그인
-          </v-card-title>
-          <v-card-actions>
-            <v-img
-              src="@/assets/images/kakao/kakao_login_large_wide.png"
-              @click="kakaologin"
-              style="cursor: pointer"
+        <!-- 비로그인 디비전 -->
+        <v-dialog v-if="!this.isLoggedIn" v-model="dialog" max-width="500">
+          <template v-slot:activator="{ on, attrs }">
+            <!-- <v-btn color="teal" dark v-bind="attrs" v-on="on"> 로그인 </v-btn> -->
+            <i v-bind="attrs" v-on="on" class="fas fa-power-off fa-lg"></i>
+          </template>
+          <v-card style="padding: 20px">
+            <v-card-title
+              class="headline d-flex justify-center ft-dh"
+              style="padding: 0px 0px 20px 0px"
             >
-            </v-img>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+              로그인
+            </v-card-title>
+            <v-card-actions>
+              <v-img
+                src="@/assets/images/kakao/kakao_login_large_wide.png"
+                @click="kakaologin"
+                style="cursor: pointer"
+              >
+              </v-img>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
-      <!-- 로그인 디비전 -->
-      <v-btn
-        v-if="this.isLoggedIn"
-        color="rgb(189, 22, 44)"
-        dark
-        @click="logout2"
+        <!-- 로그인 디비전 -->
+        <v-btn
+          v-if="this.isLoggedIn"
+          color="rgb(189, 22, 44)"
+          dark
+          @click="logout2"
+        >
+          로그아웃
+        </v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" fixed left temporary dark>
+        <v-list nav dense>
+          <v-list-item-group v-model="group">
+            <v-list-item style="margin: 12px 3px">
+              <v-list-item-title @click="$router.push('/')">
+                <h1 class="display-1 font-weight-bold">
+                  Match On
+                </h1></v-list-item-title
+              >
+            </v-list-item>
+
+            <v-list-item v-if="this.isLoggedIn" style="margin: 6px 3px">
+              <v-list-item-title @click="$router.push('/profile')">
+                <span class="text-h6 font-weight-bold">
+                  마이페이지
+                </span></v-list-item-title
+              >
+            </v-list-item>
+
+            <v-list-item style="margin: 6px 3px">
+              <v-list-item-title @click="$router.push('/howtouse')">
+                <span class="text-h6 font-weight-bold">
+                  이용방법
+                </span></v-list-item-title
+              >
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view
+          @report-detail="getReportDetail"
+          :report_detail_datas="report_detail_datas"
+          :sports_name="sports_name"
+        ></router-view>
+      </v-main>
+
+      <!-- 바텀 내비게이터 -->
+      <v-bottom-navigation
+        v-model="value"
+        background-color="black"
+        fixed="true"
       >
-        로그아웃
-      </v-btn>
-    </v-app-bar>
+        <v-btn value="home" to="/">
+          <span style="color: white">집</span>
+          <i class="fas fa-home fa-2x" style="color: white"></i>
+        </v-btn>
 
-    <v-navigation-drawer v-model="drawer" fixed left temporary dark>
-      <v-list nav dense>
-        <v-list-item-group v-model="group">
-          <v-list-item style="margin: 12px 3px">
-            <v-list-item-title @click="$router.push('/')">
-              <h1 class="display-1 font-weight-bold">
-                Match On
-              </h1></v-list-item-title
-            >
-          </v-list-item>
+        <v-btn value="match" to="/match">
+          <span style="color: white">매치</span>
+          <i class="fas fa-flag-checkered fa-2x" style="color: white"></i>
+        </v-btn>
 
-          <v-list-item v-if="this.isLoggedIn" style="margin: 6px 3px">
-            <v-list-item-title @click="$router.push('/profile')">
-              <span class="text-h6 font-weight-bold">
-                마이페이지
-              </span></v-list-item-title
-            >
-          </v-list-item>
+        <v-btn value="schedule" to="/schedule">
+          <span style="color: white">일정</span>
+          <i class="fa fa-calendar-check fa-2x" style="color: white"></i>
+        </v-btn>
 
-          <v-list-item style="margin: 6px 3px">
-            <v-list-item-title @click="$router.push('/howtouse')">
-              <span class="text-h6 font-weight-bold">
-                이용방법
-              </span></v-list-item-title
-            >
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <router-view
-        @report-detail="getReportDetail"
-        :report_detail_datas="report_detail_datas"
-        :sports_name="sports_name"
-      ></router-view>
-    </v-main>
-
-    <!-- 바텀 내비게이터 -->
-    <v-bottom-navigation v-model="value" background-color="black" fixed="true">
-      <v-btn value="home" to="/">
-        <span style="color: white">집</span>
-        <i class="fas fa-home fa-2x" style="color: white"></i>
-      </v-btn>
-
-      <v-btn value="match" to="/match">
-        <span style="color: white">매치</span>
-        <i class="fas fa-flag-checkered fa-2x" style="color: white"></i>
-      </v-btn>
-
-      <v-btn value="schedule" to="/schedule">
-        <span style="color: white">일정</span>
-        <i class="fa fa-calendar-check fa-2x" style="color: white"></i>
-      </v-btn>
-
-      <v-btn value="report" to="/report">
-        <span style="color: white">전적</span>
-        <i class="far fa-clipboard fa-2x" style="color: white"></i>
-      </v-btn>
-    </v-bottom-navigation>
-
-    <!-- <v-card style="max-width: 720px">
-      <v-btn @click="push1">push</v-btn>
-      <p>{{ token2 }}</p>
-      <p>ver 11.15.6</p>
-    </v-card> -->
+        <v-btn value="report" to="/report">
+          <span style="color: white">전적</span>
+          <i class="far fa-clipboard fa-2x" style="color: white"></i>
+        </v-btn>
+      </v-bottom-navigation>
+    </div>
+    <MainSignoutPC class="d-none d-md-block mt-15" />
   </v-app>
 </template>
 
@@ -127,6 +128,8 @@ import axios from "axios";
 import http from "@/util/http-common";
 import { mapGetters, mapState, mapActions } from "vuex";
 // import { token } from "@/services/messaging";
+import MainSignoutPC from "@/views/MainSignoutPC.vue";
+import Home from "@/views/Home.vue";
 
 export default {
   name: "app",
@@ -135,6 +138,10 @@ export default {
   //     token,
   //   };
   // },
+  components: {
+    MainSignoutPC,
+    Home,
+  },
   data() {
     return {
       dialog: false,
@@ -294,5 +301,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.h-100 {
+  height: 100%;
+}
 </style>
